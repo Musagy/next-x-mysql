8786677cfbef1a9993e02123a447638b9a5df8b8
Diego Eduardo Musagy Casas <86860902+Musagy@users.noreply.github.com>
2022-09-06 16:50:13 -0500

    actualizacion y testeo de base de datos

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,14 @@
 import { createPool } from 'mysql2/promise'
 
-// const pool = createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "1234",
-//   port: 3306,
-//   database: "productsdb",
-// })
-const pool = createPool({
+const dbConf = ({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   port: process.env.MYSQL_PORT,
   database: process.env.MYSQL_DATABASE,
 })
+
+const pool = createPool(dbConf)
+console.log(dbConf)
 
 export { pool } 
