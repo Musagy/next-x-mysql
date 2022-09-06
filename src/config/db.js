3eb508820761552cd5955c585694968d6bb676f7
Diego Eduardo Musagy Casas <86860902+Musagy@users.noreply.github.com>
2022-09-06 16:53:46 -0500

    config db

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,11 @@
 import { createPool } from 'mysql2/promise'
 
-const dbConf = ({
+const pool = createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   port: process.env.MYSQL_PORT,
   database: process.env.MYSQL_DATABASE,
 })
-
-const pool = createPool(dbConf)
-console.log(dbConf)
 
 export { pool } 
